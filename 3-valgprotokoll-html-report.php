f0a6c683b29ca3249dc00129d61eb78bf30e076e
Hallvard Nygard <hallvard.nygard@gmail.com>
2021-10-15 23:33:39 +0200

    Snåsa manually downloaded

--- 3-valgprotokoll-html-report.php
+++ 3-valgprotokoll-html-report.php
@@ -328,6 +328,7 @@ foreach ($files as $file) {
     $name2 = $name . ' kommune';
     $name2 = str_replace('Ullensvang kommune', 'Ullensvang herad', $name2);
     $name2 = str_replace('Ulvik kommune', 'Ulvik herad', $name2);
+    $name2 = str_replace('Snåase - Snåsa kommune', 'Snåsa kommune', $name2);
     if ($obj->county == 'Viken') {
         $name2 = str_replace('Våler kommune', 'Våler kommune (Østfold)', $name2);
     }
