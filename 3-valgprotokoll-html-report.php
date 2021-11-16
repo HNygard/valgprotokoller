cb39689042ef306840da5e60b44f31579e9347d2
Hallvard Nygard <hallvard.nygard@gmail.com>
2021-11-16 23:23:30 +0100

    Emails

--- 3-valgprotokoll-html-report.php
+++ 3-valgprotokoll-html-report.php
@@ -356,9 +356,11 @@ foreach ($files as $file) {
     $name2 = $name . ' kommune';
     //$name2 = str_replace('Ullensvang kommune', 'Ullensvang herad', $name2);
     $name2 = str_replace('Ulvik kommune', 'Ulvik herad', $name2);
+    $name2 = str_replace('Kvam kommune', 'Kvam herad', $name2);
     $name2 = str_replace('Snåase - Snåsa kommune', 'Snåsa kommune', $name2);
     $name2 = str_replace('Gáivuotna - Kåfjord - Kaivuono kommune', 'Kåfjord kommune', $name2);
     $name2 = str_replace('Storfjord - Omasvuotna - Omasvuono kommune', 'Storfjord kommune', $name2);
+    $name2 = str_replace('Raarvihke - Røyrvik kommune', 'Røyrvik kommune', $name2);
     if ($obj->county == 'Viken') {
         $name2 = str_replace('Våler kommune', 'Våler kommune (Østfold)', $name2);
     }
